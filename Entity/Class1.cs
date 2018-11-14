8ddb5af08f04f1142b0c9f266691621d15240a58
LJL <781867500@qq.com>
2018-11-14 12:12:31 +0800

    搭建项目

---
+++ Entity/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WXManagement.Entity
+{
+    public class Class1
+    {
+    }
+}
---
+++ WXManagementUnitTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WXManagement.CommonUtility;
+
+namespace WXManagementUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            wxapi apWxapi = new wxapi();
+            //HttpRequest request = new HttpRequest("","www.baidu.com","");
+            //apWxapi.ProcessRequest(new HttpContext(request,new HttpResponse()));
+        }
+    }
+}
